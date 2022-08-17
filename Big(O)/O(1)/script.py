8105609c79f8c3ee9562f0b79c6d6125b0398c6b
athena272 <guilhermera272@gmail.com>
2022-08-17 15:07:37 -0300

    Create script.py

---
+++ Big(O)/O(1)/script.py
@@ -0,0 +1,20 @@
+import queue
+
+
+def example1():
+    print("Hoya World")
+
+
+def example2(number):
+    return number
+
+
+def example3(data):
+    return data[0]
+
+
+def example4():
+    myQueue = queue.Queue()
+    myQueue.put(1)
+
+    
