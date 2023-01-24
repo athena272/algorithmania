bb1a0b4897c70d0e90d33cadf4106b5bda43b19f
athena272 <guilhermera272@gmail.com>
2023-01-24 11:29:22 -0300

    Update stack.py

--- DataStructure/003_Stack/stack.py
+++ DataStructure/003_Stack/stack.py
@@ -3,6 +3,37 @@ from linked_list import LinkedList, Node
 import unittest
 
 
+def print_linked_list(linked_list):
+    # 1 -> 2 -> 3 -> 4 -> 5 -> None
+    node = linked_list.head()
+    while node:
+        print(node.value, end=" ")
+        node = node.next
+    print("")
+
+
+def print_reversed_linked_list(linked_list):
+    stack = []
+    node = linked_list.head()
+    while node:
+        stack.append(node.value)
+        node = node.next
+
+    while stack:
+        print(stack.pop(), end=" ")
+    print("")
+
+
 class LinkedListTest(unittest.TestCase):
     def setUp(self):
         self.linked_list = LinkedList()
+
+    def test_reversed_list(self):
+        self.linked_list.insert_node_to_tail(Node('1'))
+        self.linked_list.insert_node_to_tail(Node('2'))
+        self.linked_list.insert_node_to_tail(Node('3'))
+        self.linked_list.insert_node_to_tail(Node('4'))
+        self.linked_list.insert_node_to_tail(Node('5'))
+
+        print_linked_list(self.linked_list)
+        print_reversed_linked_list(self.linked_list)
