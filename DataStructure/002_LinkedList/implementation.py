2b816b48cdf9a3d0c556e5c2ae14f9a2b00e8fe4
athena272 <guilhermera272@gmail.com>
2023-01-24 08:45:48 -0300

    Update implementation.py

--- DataStructure/002_LinkedList/implementation.py
+++ DataStructure/002_LinkedList/implementation.py
@@ -1,42 +1,40 @@
-# import unittest
+from resolution import LinkedList, Node
 
-# class Node:
-#     def __init__(self, value):
-#         self.next = None
-#         self.value = value
+import unittest
 
-# class LinkedList(unittest.TestCase):
-#     def setUp(self):
-#         self.linked_list = LinkedList()
 
-#     def test_insert_first_node_to_tail(self):
-#         self.linked_list.insert_node_to_tail(Node('tail'))
-#         self.assertEqual('tail', self.linked_list.tail().value)
+class LinkedListTest(unittest.TestCase):
+    def setUp(self):
+        self.linked_list = LinkedList()
 
-#     def test_insert_first_node_to_head(self):
-#         self.linked_list.insert_node_to_head(Node('head'))
-#         self.assertEqual('head', self.linked_list.head().value)
+    def test_insert_first_node_to_tail(self):
+        self.linked_list.insert_node_to_tail(Node('tail'))
+        self.assertEqual('tail', self.linked_list.tail().value)
 
-#     def test_insert_two_nodes_to_head(self):
-#         self.linked_list.insert_node_to_head(Node('head2'))
-#         self.linked_list.insert_node_to_head(Node('head1'))
-#         self.assertEqual('head1', self.linked_list.head().value)
+    def test_insert_first_node_to_head(self):
+        self.linked_list.insert_node_to_head(Node('head'))
+        self.assertEqual('head', self.linked_list.head().value)
 
-#     def test_insert_two_nodes_to_tail(self):
-#         self.linked_list.insert_node_to_tail(Node('tail2'))
-#         self.linked_list.insert_node_to_tail(Node('tail1'))
-#         self.assertEqual('tail1', self.linked_list.tail().value)
+    def test_insert_two_nodes_to_head(self):
+        self.linked_list.insert_node_to_head(Node('head2'))
+        self.linked_list.insert_node_to_head(Node('head1'))
+        self.assertEqual('head1', self.linked_list.head().value)
 
-#     def test_insert_nodes_to_head_and_tail(self):
-#         self.linked_list.insert_node_to_head(Node('head'))
-#         self.linked_list.insert_node_to_tail(Node('tail'))
-#         self.assertEqual('head', self.linked_list.head().value)
-#         self.assertEqual('tail', self.linked_list.tail().value)
+    def test_insert_two_nodes_to_tail(self):
+        self.linked_list.insert_node_to_tail(Node('tail2'))
+        self.linked_list.insert_node_to_tail(Node('tail1'))
+        self.assertEqual('tail1', self.linked_list.tail().value)
 
-#     def test_is_empty_with_empty_linked_list(self):
-#         self.assertTrue(self.linked_list.is_empty())
+    def test_insert_nodes_to_head_and_tail(self):
+        self.linked_list.insert_node_to_head(Node('head'))
+        self.linked_list.insert_node_to_tail(Node('tail'))
+        self.assertEqual('head', self.linked_list.head().value)
+        self.assertEqual('tail', self.linked_list.tail().value)
 
-#     def test_is_empty_with_two_nodes(self):
-#         self.linked_list.insert_node_to_head(Node('element'))
-#         self.linked_list.insert_node_to_head(Node('element2'))
-#         self.assertFalse(self.linked_list.is_empty())
+    def test_is_empty_with_empty_linked_list(self):
+        self.assertTrue(self.linked_list.is_empty())
+
+    def test_is_empty_with_two_nodes(self):
+        self.linked_list.insert_node_to_head(Node('element'))
+        self.linked_list.insert_node_to_head(Node('element2'))
+        self.assertFalse(self.linked_list.is_empty())
