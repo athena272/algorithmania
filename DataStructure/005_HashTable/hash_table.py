bf906d12f7a5d7ddeba249e07b1d9d01b0429323
athena272 <guilhermera272@gmail.com>
2023-01-25 22:35:47 -0300

    Update hash_table.py

--- DataStructure/005_HashTable/hash_table.py
+++ DataStructure/005_HashTable/hash_table.py
@@ -1,6 +1,6 @@
-def calc_hash(item, item_cout):
+def calc_hash(item, item_count):
     hash = 0
     for c in item:
         hash += ord(c)
     
-    return hash % item_cout   
+    return hash % item_count   
