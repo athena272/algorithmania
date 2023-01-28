12d09f235b41fe78adaa32a8e886376a65c0f3bd
athena272 <guilhermera272@gmail.com>
2023-01-28 20:29:50 -0300

    Update compare_binary_tree_bfs.py

--- 003_Desafios/000_Basico/000_CompareBinaryTree/compare_binary_tree_bfs.py
+++ 003_Desafios/000_Basico/000_CompareBinaryTree/compare_binary_tree_bfs.py
@@ -1,6 +1,10 @@
 from binary_tree import BinaryTree
 
 
+def line():
+    print("=+=" * 20)
+
+
 def compare_binary_tree_bfs(tree1, tree2):
     queue = [(tree1, tree2)]
 
@@ -16,8 +20,37 @@ def compare_binary_tree_bfs(tree1, tree2):
         if t1.data != t2.data:
             return False
 
-        queue.append(t1.left, t2.left)
-        queue.append(t1.right, t2.right)
-
+        queue.append((t1.left, t2.left))
+        queue.append((t1.right, t2.right))
 
     return True
+
+
+# Test 1
+tree1 = BinaryTree()
+tree1.insert(1)
+tree1.insert(2)
+tree1.insert(3)
+
+tree2 = BinaryTree()
+tree2.insert(1)
+tree2.insert(2)
+tree2.insert(3)
+
+print(compare_binary_tree_bfs(tree1.root, tree2.root))
+line()
+
+# Test 2
+tree1 = BinaryTree()
+tree1.insert(1)
+tree1.insert(2)
+tree1.insert(3)
+
+tree2 = BinaryTree()
+tree2.insert(1)
+tree2.insert(2)
+tree2.insert(4)
+tree2.insert(5)
+tree2.insert(3)
+
+print(compare_binary_tree_bfs(tree1.root, tree2.root))
