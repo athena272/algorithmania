5169ab8c5233c129becfc43f3d5e21841f5ecb5a
athena272 <guilhermera272@gmail.com>
2023-01-28 17:27:57 -0300

    Create test_dynamic_fibo.py

---
+++ 002_Heuristica/003_DynamicProgramming/test_dynamic_fibo.py
@@ -0,0 +1,18 @@
+import unittest
+
+from dynamic_fibonacci import dynamic_fibonacci
+
+
+class MyTest(unittest.TestCase):
+    def test_example_checks(self):
+        self.assertEqual(1, dynamic_fibonacci(1))
+        self.assertEqual(1, dynamic_fibonacci(2))
+
+    def test_other_checks(self):
+        self.assertEqual(2, dynamic_fibonacci(3))
+        self.assertEqual(3, dynamic_fibonacci(4))
+        self.assertEqual(5, dynamic_fibonacci(5))
+        self.assertEqual(8, dynamic_fibonacci(6))
+
+    def test_slow_tests(self):
+        self.assertEqual(9227465, dynamic_fibonacci(35))
