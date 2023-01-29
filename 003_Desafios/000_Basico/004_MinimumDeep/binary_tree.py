b52ef2db494fd3180c588b2c110fbacdfb10eb1b
athena272 <guilhermera272@gmail.com>
2023-01-29 16:32:51 -0300

    Create binary_tree.py

---
+++ 003_Desafios/000_Basico/004_MinimumDeep/binary_tree.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        if self.root is None:
+            self.root = new_node
+        else:
+            current = self.root
+            while True:
+                if data < current.data:
+                    if current.left is None:
+                        current.left = new_node
+                        break
+                    else:
+                        current = current.left
+                else:
+                    if current.right is None:
+                        current.right = new_node
+                        break
+                    else:
+                        current = current.right
+
+
+def print_tree(root):
+    if root:
+        print(root.data)
+        print_tree(root.left)
+        print_tree(root.right)
