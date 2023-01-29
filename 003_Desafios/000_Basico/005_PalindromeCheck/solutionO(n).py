8af08f8253f772c78d99f640aee0a729e571787f
athena272 <guilhermera272@gmail.com>
2023-01-29 16:33:03 -0300

    Create solutionO(n).py

---
+++ 003_Desafios/000_Basico/005_PalindromeCheck/solutionO(n).py
@@ -0,0 +1,24 @@
+# tempo O(n)
+# espaco/memoria O(n)
+
+def manually_way(string):
+    string_reverse = ''
+    for index in reversed(range(len(string))):
+        string_reverse += string[index]
+
+    return string_reverse == string
+
+
+def reverse_string(string):
+    string_reverse = string[::-1]
+
+    return string_reverse == string
+
+
+def another_reverse_string(string):
+    string_reverse = ''.join(reversed(string))
+
+    return string_reverse == string
+
+
+print(manually_way("bab"))
