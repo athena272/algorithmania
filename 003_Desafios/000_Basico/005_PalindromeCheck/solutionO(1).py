14887585875c14be2a888a5b1f31412f4a3dface
athena272 <guilhermera272@gmail.com>
2023-01-29 18:25:23 -0300

    Update solutionO(1).py

--- 003_Desafios/000_Basico/005_PalindromeCheck/solutionO(1).py
+++ 003_Desafios/000_Basico/005_PalindromeCheck/solutionO(1).py
@@ -4,7 +4,18 @@
 def best_palindrome(string):
     last_index = len(string) - 1
     for index in range(len(string)):
+        if last_index == index:
+            return True
+
         if string[index] != string[last_index]:
             return False
 
         last_index -= 1
+
+    return True
+
+print(best_palindrome('abcba'))
+
+print(best_palindrome('level'))
+
+print(best_palindrome('agjgwneogoa'))
